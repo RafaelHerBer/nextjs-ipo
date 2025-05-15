9f22afd8607fbe6d307695d56a72952c768e7f5c
rafa <franciscorafaelherrero@gmail.com>
2025-05-15 04:09:22 +0200

    Added path for search, not yer implemented

---
+++ src/app/search/page.tsx
@@ -0,0 +1,10 @@
+import { Text } from "@radix-ui/themes"
+
+const Page = ()=>{
+    return (
+        <>
+            <Text> Search Not implemented yet </Text>
+        </>
+    )
+}
+export default Page
